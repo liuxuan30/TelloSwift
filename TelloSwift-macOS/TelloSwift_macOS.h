4b802c387c1dfe7916bd0dc8207aab454dc32432
Xuan Liu <liuxuan30@gmail.com>
2019-11-29 22:47:59 +0800

    add macOS test target change code sign for macOS

---
+++ TelloSwift-macOS/TelloSwift_macOS.h
@@ -0,0 +1,19 @@
+//
+//  TelloSwift_macOS.h
+//  TelloSwift-macOS
+//
+//  Created by Xuan Liu on 2019/11/29.
+//  Copyright © 2019 Xuan Liu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TelloSwift_macOS.
+FOUNDATION_EXPORT double TelloSwift_macOSVersionNumber;
+
+//! Project version string for TelloSwift_macOS.
+FOUNDATION_EXPORT const unsigned char TelloSwift_macOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TelloSwift_macOS/PublicHeader.h>
+
+
